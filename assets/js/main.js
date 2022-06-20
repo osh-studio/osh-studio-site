f6ab9697ee9db3c5cd90d49b94e991a6ec51af45
ben <ben.f@iterateagency.com>
2022-06-20 11:05:19 +1000

    email testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-function formSubmit(event) {
-    var url = "https://studio.us13.list-manage.com/subscribe/post?u=b6e6e8aafcf46407b042fba60&amp;id=902ad9814c";
-    var request = new XMLHttpRequest();
-    request.open('POST', url, true);
-    request.onload = function() { // request successful
-    // we can use server response to our request now
-      console.log(request.responseText);
-    };
+// function formSubmit(event) {
+//     var url = "https://studio.us13.list-manage.com/subscribe/post?u=b6e6e8aafcf46407b042fba60&amp;id=902ad9814c";
+//     var request = new XMLHttpRequest();
+//     request.open('POST', url, true);
+//     request.onload = function() { // request successful
+//     // we can use server response to our request now
+//       console.log(request.responseText);
+//     };
   
-    request.onerror = function() {
-      // request failed
-    };
+//     request.onerror = function() {
+//       // request failed
+//     };
   
-    request.send(new FormData(event.target)); // create FormData from form that triggered event
-    event.preventDefault();
-  }
+//     request.send(new FormData(event.target)); // create FormData from form that triggered event
+//     event.preventDefault();
+//   }
   
-document.getElementById('mc-embedded-subscribe-form').addEventListener("submit", formSubmit);
+// document.getElementById('mc-embedded-subscribe-form').addEventListener("submit", formSubmit);
