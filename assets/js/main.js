f101b611512042923728dbd36da01d9cfcfd2992
ben <ben.f@iterateagency.com>
2022-06-20 11:07:22 +1000

    more form testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
-// function formSubmit(event) {
-//     var url = "https://studio.us13.list-manage.com/subscribe/post?u=b6e6e8aafcf46407b042fba60&amp;id=902ad9814c";
-//     var request = new XMLHttpRequest();
-//     request.open('POST', url, true);
-//     request.onload = function() { // request successful
-//     // we can use server response to our request now
-//       console.log(request.responseText);
-//     };
-  
-//     request.onerror = function() {
-//       // request failed
-//     };
-  
-//     request.send(new FormData(event.target)); // create FormData from form that triggered event
-//     event.preventDefault();
-//   }
-  
-// document.getElementById('mc-embedded-subscribe-form').addEventListener("submit", formSubmit);
+$(document).ready( function () {
+    // I only have one form on the page but you can be more specific if need be.
+    var $form = $('form');
+
+    if ( $form.length > 0 ) {
+        $('form input[type="submit"]').bind('click', function ( event ) {
+            if ( event ) event.preventDefault();
+            // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+            if ( validate_input($form) ) { register($form); }
+        });
+    }
+});
+
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+                // Something went wrong, do something to notify the user. maybe alert(data.msg);
+            } else {
+                // It worked, carry on...
+            }
+        }
+    });
+}
