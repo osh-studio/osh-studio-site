cef4a26bb94831981edd0126bed1f5856d0adc72
ben <ben.f@iterateagency.com>
2022-06-20 10:51:51 +1000

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,18 @@
-import '../css/style.css'
-import '../css/fonts.css'
+function formSubmit(event) {
+    var url = "https://studio.us13.list-manage.com/subscribe/post?u=b6e6e8aafcf46407b042fba60&amp;id=902ad9814c";
+    var request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.onload = function() { // request successful
+    // we can use server response to our request now
+      console.log(request.responseText);
+    };
+  
+    request.onerror = function() {
+      // request failed
+    };
+  
+    request.send(new FormData(event.target)); // create FormData from form that triggered event
+    event.preventDefault();
+  }
+  
+document.getElementById('mc-embedded-subscribe-form').addEventListener("submit", formSubmit);
